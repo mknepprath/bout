d3d4c82b1eba230a499ddc3b9cee18a12c8c55db
mknepprath <mknepprath@gmail.com>
2017-04-09 13:31:43 -0500

    Add node-cron to speed things up - runs every 15s instead of 10min

--- clock.js
+++ clock.js
@@ -1,35 +1,23 @@
-const CronJob = require('cron').CronJob
-const childProcess = require('child_process')
+const cron = require('node-cron')
+const process = require('process')
+const child_process = require('child_process')
 
-new CronJob({
-  cronTime: '1 * * * *', //15 seconds after every minute
-  onTick: runBout('./app.js', function (err) {
-    if (err) throw err
-    console.log('Finished running app.js')
-  }),
-  start: true
+const simple_log = process.argv.indexOf('--simple') > -1
+
+cron.schedule('0,15,30,45 * * * * *', function () {
+  var exec = child_process.exec, child
+
+  child = exec('node app.js {{args}}',
+    function (error, stdout, stderr) {
+      if (simple_log) {
+        console.log('Ping!')
+      } else {
+        console.log('=================')
+        console.log('* RAN FROM CRON *')
+        console.log('=================')
+        console.log(stdout)
+      }
+      if (stderr) console.log('stderr: ' + stderr)
+      if (error !== null) console.log('exec error: ' + error)
+  })
 })
-
-function runBout(scriptPath, callback) {
-
-  // keep track of whether callback has been invoked to prevent multiple invocations
-  let invoked = false
-
-  const process = childProcess.fork(scriptPath)
-
-  // listen for errors as they may prevent the exit event from firing
-  process.on('error', function (err) {
-    if (invoked) return
-    invoked = true
-    callback(err)
-  })
-
-  // execute the callback once the process has finished running
-  process.on('exit', function (code) {
-    if (invoked) return
-    invoked = true
-    const err = code === 0 ? null : new Error('exit code ' + code)
-    callback(err)
-  })
-
-}
